f100b65dc57458be921cadcc39c209953b910f4e
Carlos <carlosmonzonis@outlook.es>
2020-06-17 19:17:21 +0200

    added velocity damping to simulation.

--- src/simulation/pbdsimulation.cpp
+++ src/simulation/pbdsimulation.cpp
@@ -17,14 +17,48 @@ namespace ucloth{
 
          void PBDSimulation::dampVelocities(std::vector<Mesh> const& meshes, std::vector<umath::Position> const& positions, std::vector<umath::Real> const& inverseMasses, std::vector<umath::Vec3>& velocities){
              for(auto const& mesh : meshes){
-                umath::Real const total_mass = 
+                // TODO: optimaze mass calculations
+                // Total mass
+                umath::Real const totalMass = 
                     std::accumulate(inverseMasses.begin() + mesh.begin, 
                                     inverseMasses.begin() + mesh.end, 
                                     0.0f,
                                     [](umath::Real input, umath::Real const inverseMass) -> umath::Real { 
                                         return input + (1.0f / inverseMass); 
                                     });
-             }
+
+                // Calculate center of mass and its velocity
+                umath::Position xcm = {0.0f, 0.0f, 0.0f};
+                umath::Vec3 vcm = {0.0f, 0.0f, 0.0f};
+                for(Particle p = mesh.begin; p < mesh.end; ++p){
+                    xcm += positions[p] / (inverseMasses[p]);
+                    vcm += velocities[p] / (inverseMasses[p]);
+                }
+                xcm /= totalMass;
+                vcm /= totalMass;
+
+                umath::Vec3 L = {0.0f, 0.0f, 0.0f};
+                for(Particle p = mesh.begin; p < mesh.end; ++p){
+                    umath::Position const ri = positions[p] - xcm;
+                    L += umath::cross(ri, velocities[p] / inverseMasses[p]);
+                }
+                
+                //Calculate inertia matrix
+                umath::Mat3x3 I = umath::Mat3x3{0.0f};
+                for(Particle p = mesh.begin; p < mesh.end; ++p){
+                    umath::Position const ri = positions[p] - xcm;
+                    // Column major
+                    umath::Mat3x3 const riPrime = {{0, -ri.z, ri.y}, {ri.z, 0, -ri.x}, {-ri.y, ri.x, 0}};
+                    I += riPrime * umath::transpose(riPrime) / inverseMasses[p];
+                }
+
+                umath::Vec3 const angularVelocity = umath::inverse(I) * L;
+                for(Particle p = mesh.begin; p < mesh.end; ++p){
+                    umath::Position const ri = positions[p] - xcm;
+                    umath::Vec3 const deltaV = vcm + umath::cross(angularVelocity, ri) - velocities[p];
+                    velocities[p] += mesh.kVelocity * deltaV;
+                }
+            }
          }
     } // namespace simulation
 } // namespace ucloth
